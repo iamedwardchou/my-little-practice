2df49c8b55a30157e864f7bf9aafc0c2ed715e62
sheng-wei12 <a15361222@gmail.com>
2022-02-13 23:09:37 +0800

    set localstorage

--- src/components/Search/ShowRoute.js
+++ src/components/Search/ShowRoute.js
@@ -7,110 +7,22 @@ const ShowRoute = ({ city, routeName, depName, desName, setCurrentRender }) => {
   // const [arrivalTime,setArrivalTime] = useState([])
   const [reFetch, setReFetch] = useState(false);
   const [status, setStatus] = useState("back");
-  const [goData, setGoData] = useState([]);
-  const [backData, setBackData] = useState([]);
+
+  const [goData, setGoData] = useState(() =>{
+    const savedGo = localStorage.getItem("goData")
+    const intialGo = JSON.parse(savedGo)
+    return intialGo || [];
+  });
+  const [backData, setBackData] = useState(()=>{
+    const savedBack = localStorage.getItem("backData")
+    const intialBack = JSON.parse(savedBack)
+    return intialBack || []
+  });
 
   const handleRefetch = () => {
     fetchData();
     setReFetch(true);
   };
-
-  const fetchData = useCallback(() => {
-    let goRouteData = [];
-    let backRouteData = [];
-    let stopData = [];
-    console.log(status);
-    //看來只能先處理完第一次呼叫，再處理下一次呼叫
-    async function getBus() {
-      try {
-        let res = await apiBusRoute(city, routeName);
-        if (res.status === 200) {
-          console.log(res.status);
-        }
-        if (status === "go") {
-          console.log("go");
-          goRouteData = getGoRoute(res.data);
-          getRoute(goRouteData, status);
-        } else if (status === "back") {
-          console.log("back");
-          backRouteData = getBackRoute(res.data);
-          getRoute(backRouteData, status);
-        }
-      } catch (err) {
-        console.error(err);
-      }
-    }
-
-    async function getRoute(routeData, status) {
-      try {
-        let res = await apiBusStopRoute(city, routeName);
-        if (res === 200) {
-          console.log(res.status);
-        }
-        if (status === "go") {
-          console.log("goStops");
-          stopData = getGoStop(res.data);
-        } else if (status === "back") {
-          console.log("backStops");
-          stopData = getBackStop(res.data);
-        }
-        // console.log(stopData)
-        // console.log(routeData)
-
-        let time = 0;
-        let timeText = "";
-        let dataTest = [];
-
-        stopData.forEach((stop) => {
-          routeData.forEach((route) => {
-            let dataObj = {};
-            if (route.stopUID === stop.StopUID) {
-              time = Math.floor(route.estimateTime / 60);
-              // 文字顯示
-              if (time === 0) {
-                timeText = "進站中";
-              } else if (time <= 1 && 0 < time) {
-                timeText = "即將到站";
-              } else if (!time) {
-                timeText = "--";
-              } else {
-                timeText = `${time} 分鐘`;
-              }
-              dataObj.stopUID = stop.StopUID;
-              dataObj.time = timeText;
-              dataObj.stopName = stop.StopName.Zh_tw;
-              dataTest.push(dataObj);
-              // console.log(dataTest)
-            }
-          });
-        });
-        // 再來設定各種set 再帶到去返程組件渲染
-        if (status === "back") {
-          setBackData(dataTest);
-        } else if (status === "go") setGoData(dataTest);
-      } catch (err) {
-        console.error(err);
-      }
-    }
-
-    getBus();
-    setReFetch(false);
-  }, [status]);
-
-  // 還需要再加useEffect
-
-  // 將去回程資料轉換成 function 寫在這裡就好
-  useEffect(() => {
-    console.log("execute function in useEffect");
-    fetchData();
-  }, [fetchData]);
-
-  // 在這裡處理非同步回傳的資料
-  // useEffect(() => {
-  //     console.log(busRoute)
-  //     console.log(stopRouteData)
-  //     // getBackRoute()
-  // },[busRoute,stopRouteData])
 
   function getGoStop(stopRouteData) {
     // 選取站數最多的為路線列表
@@ -179,6 +91,106 @@ const ShowRoute = ({ city, routeName, depName, desName, setCurrentRender }) => {
     return busBackData;
   }
 
+  const fetchData = useCallback(() => {
+    let goRouteData = [];
+    let backRouteData = [];
+    let stopData = [];
+    console.log(status);
+    //看來只能先處理完第一次呼叫，再處理下一次呼叫
+    async function getBus() {
+      try {
+        let res = await apiBusRoute(city, routeName);
+        if (res.status === 200) {
+          console.log(res.status);
+        }
+        if (status === "go") {
+          console.log("go");
+          goRouteData = getGoRoute(res.data);
+          getRoute(goRouteData, status);
+        } else if (status === "back") {
+          console.log("back");
+          backRouteData = getBackRoute(res.data);
+          getRoute(backRouteData, status);
+        }
+      } catch (err) {
+        console.error(err);
+      }
+    }
+
+    async function getRoute(routeData, status) {
+      try {
+        let res = await apiBusStopRoute(city, routeName);
+        if (res === 200) {
+          console.log(res.status);
+        }
+        if (status === "go") {
+          console.log("goStops");
+          stopData = getGoStop(res.data);
+        } else if (status === "back") {
+          console.log("backStops");
+          stopData = getBackStop(res.data);
+        }
+        let time = 0;
+        let timeText = "";
+        let dataTest = [];
+
+        stopData.forEach((stop) => {
+          routeData.forEach((route) => {
+            let dataObj = {};
+            if (route.stopUID === stop.StopUID) {
+              time = Math.floor(route.estimateTime / 60);
+              // 文字顯示
+              if (time === 0) {
+                timeText = "進站中";
+              } else if (time <= 1 && 0 < time) {
+                timeText = "即將到站";
+              } else if (!time) {
+                timeText = "--";
+              } else {
+                timeText = `${time} 分鐘`;
+              }
+              dataObj.stopUID = stop.StopUID;
+              dataObj.time = timeText;
+              dataObj.stopName = stop.StopName.Zh_tw;
+              dataTest.push(dataObj);
+              // console.log(dataTest)
+            }
+          });
+        });
+        // 再來設定各種set 再帶到去返程組件渲染
+        if (status === "back") {
+          setBackData(dataTest);
+        } else if (status === "go") setGoData(dataTest);
+      } catch (err) {
+        console.error(err);
+      }
+    }
+    getBus();
+    setReFetch(false);
+  }, [status]);
+
+  useEffect(() => {
+    console.log("execute function in useEffect");
+    fetchData();
+  }, [fetchData]);
+
+  useEffect(() => {
+    localStorage.setItem("goData", JSON.stringify(goData))
+  }, [goData]);
+
+  useEffect(() => {
+    localStorage.setItem("backData", JSON.stringify(backData))
+  }, [backData]);
+  // 先在dependencies 這裡確定要儲存的goData 和 bakcData, 要先JSON.stringfy
+  // 再到一開始 useState的地方取得 parse後的 data
+
+  // 在這裡處理非同步回傳的資料
+  // useEffect(() => {
+  //     console.log(busRoute)
+  //     console.log(stopRouteData)
+  //     // getBackRoute()
+  // },[busRoute,stopRouteData])
+
   // 再配合即時路線資訊渲染到站時間
   // console.log(maxStopsNum, stopIndex)
   // let stop = Math.max.apply(Math, stopRouteBack.map((route) => route.Stops.length))
@@ -210,28 +222,6 @@ const ShowRoute = ({ city, routeName, depName, desName, setCurrentRender }) => {
           goData &&
           goData.map((data) => <li key={data.stopUID}>{data.stopName}</li>)}
       </ul>
-      {/* IIFE是什麼 
-            沒有設定到go + 沒有渲染*/}
-      {/* <ul>
-            {(() => {
-                 if (status === "back") {
-                    backData &&
-                    backData.map(data =>(
-                        <li key={1}>
-                            {data.stopName}
-                        </li>
-                ))
-               }
-                else if (status === "go"){
-                    goData &&
-                    goData.map(data =>(
-                        <li key={1}>
-                            {data.stopName}
-                        </li>
-                ))
-                }
-                })()}
-            </ul> */}
 
       <button
         type="button"
@@ -246,7 +236,7 @@ const ShowRoute = ({ city, routeName, depName, desName, setCurrentRender }) => {
 };
 
 const SearchNav = ({ depName, desName, setStatus, fetchData }) => {
-    //不用useRef 好像也沒關係 
+  //不用useRef 好像也沒關係
   const statusValue = useRef(null);
   const handleSelectRoute = (e) => {
     setStatus(e.target.value);
